fdb6fb39fe1476b15a9bcea82bbd1603a22c0e0d
Landon Pierce <landonpierce@microsoft.com>
2022-09-16 15:51:47 -0400

    init function

--- B2cCli.psm1
+++ B2cCli.psm1
@@ -26,6 +26,68 @@ function Connect-B2cCli {
 
 }
 
+function Initialize-B2cIdentityExperienceFramework {
+    <#
+    .SYNOPSIS
+    Initializes the Identity Experience Framework. 
+    .DESCRIPTION
+    Initializes the B2C tenant Identity Experience Framework and prepares it for Custom Policy use. Follows the workflow laid out in: https://docs.microsoft.com/en-us/azure/active-directory-b2c/tutorial-create-user-flows?pivots=b2c-custom-policy
+    #>
+
+    New-B2cTrustFrameworkKey -Kty "RSA" -Use "sig" -KeySetName "TokenSigningKeyContainer"
+    New-B2cTrustFrameworkKey -Kty "RSA" -Use "enc" -KeySetName "TokenEncryptionKeyContainer"
+}
+
+function New-B2cAppRegistration {
+<#
+.SYNOPSIS
+Creates an app registration in the B2C tenant. 
+
+.DESCRIPTION
+Creates an app registration in the B2C tenant. Returns an object containing the app registrations properties, the service principal's properties, and the client secret (if created).  
+
+.EXAMPLE
+An example
+
+.NOTES
+General notes
+#>
+}
+
+##########################################################
+################## Helper Functions ######################
+##########################################################
+
+## TODO: Add the ability to add more than just signing/encryption keys (ie add secrets)
+function New-B2cTrustFrameworkKey{
+    <#
+    .SYNOPSIS 
+    Wrapper for New-MgTrustFrameworkKeySet and New-MgTrustFrameworkKeySetKey
+    .PARAMETER Kty
+    Key Type Parameter
+    .PARAMETER Use
+    Key Use Parameter
+    .PARAMETER KeySetName
+    The name of the key set to create
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Kty,
+        [Parameter(Mandatory=$true)]
+        [string]$Use,
+        [Parameter(Mandatory=$true)]
+        [string]$KeySetName
+    )
+
+    Write-Host "Creating Key Set: $KeySetName"
+
+    $trustFrameworkKeySet = New-MgTrustFrameworkKeySet -Id $KeySetName
+    New-MgTrustFrameworkKeySetKey -B2cTrustFrameworkKeySetId $trustFrameworkKeySet.Id -Kty $Kty -Use $Use 
+    
+    return $trustFrameworkKeySetName
+
+}
 function Get-MgGraphStatus {
     <#
     .SYNOPSIS
